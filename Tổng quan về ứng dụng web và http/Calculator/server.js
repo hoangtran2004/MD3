07a5d08d726bdf822aa1f1358fa9242ab6903ce6
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-10-11 09:50:13 +0700

    done register

---
+++ Tổng quan về ứng dụng web và http/Calculator/server.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const qs=require('qs');
+const fs=require('fs');
+http.createServer(function (req, res){
+    if (req.method==='GET'){
+        fs.readFile('')
+    }
+})
