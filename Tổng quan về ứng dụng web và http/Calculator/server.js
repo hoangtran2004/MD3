375a8e7c6cfba93d4dd4edcf2b99b5f5e0abf4bd
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-10-11 10:11:47 +0700

    register

--- Tổng quan về ứng dụng web và http/Calculator/server.js
+++ Tổng quan về ứng dụng web và http/Calculator/server.js
@@ -1,8 +1,31 @@
-const http=require('http');
-const qs=require('qs');
-const fs=require('fs');
-http.createServer(function (req, res){
-    if (req.method==='GET'){
-        fs.readFile('')
+const http = require('http');
+const qs = require('qs');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    if (req.method === 'GET') {
+        fs.readFile('./Calculator/calculator.html', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end()
+        });
+    } else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            const number = qs.parse(data);
+            fs.readFile('./Calculator/result', 'utf-8', function (err, datahtml) {
+                if (err) {
+                    console.log(err)
+                }
+                if (number.select) {
+                if (number.select==="addition"){
+                    datahtml=datahtml.replace('{result}',Number(number.number))
+                }
+                }
+            })
+        })
     }
 })
